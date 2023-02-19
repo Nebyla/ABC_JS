cc4538a574cd9335ff46949afe6e7148c53a3d1e
эрвин бекиров <tom.margan@mail.ru>
2023-02-19 13:41:44 +0300

    200 упражнение

--- Users.js
+++ Users.js
@@ -1,8 +1,5 @@
 let arr = [1, 2, 3, 4, 5];
-let sum = 0;
+console.log( arr.filter(elem => elem % 2 == 0).length );
 
-arr.forEach(function(elem) {
-	sum += elem * elem;
-});
-
-console.log(sum);
+let arr1 = [1, 2, 3, 4, 5];
+console.log( arr.filter(elem => elem % 2 == 0) );
