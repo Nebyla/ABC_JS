a9cd106d3e3ae39f09df4e05895739fe760b632a
эрвин бекиров <tom.margan@mail.ru>
2023-02-19 13:24:10 +0300

    196 упражнение

--- Users.js
+++ Users.js
@@ -1,7 +1,11 @@
-let result = filter([1, 2, 3, 4, 5], elem => elem % 2 == 0);
+function getSum(arr) {
+	let sum = arr.shift();
+	
+	if (arr.length !== 0) {
+		sum *= getSum(arr);
+	}
+	
+	return sum;
+}
 
-let result1 = every([1, 2, 3, 4, 5], (elem, index) => (elem * index > 10));
-
-let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-
-let result2 = each(arr, (elem, index) => (elem * index > 10))
+console.log(getSum([1, 2, 3, 4, 5]));
