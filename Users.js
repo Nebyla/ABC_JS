236f0993c8d1a75006fe0362bb7d98e1b2186ad3
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-02-21 18:57:09 +0300

    218 упражнение

--- Users.js
+++ Users.js
@@ -1,9 +1,8 @@
 let options = {
-	color: 'red',
 	width:  400,
 	height: 500,
 };
 
-let {color: c, width: w, height: h} = options;
+let {color = 'black', width: w, height: h} = options;
 
-console.log(h)
+console.log(color)
