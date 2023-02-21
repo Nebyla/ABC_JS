31bfc65077ebb777020d386ace47163de3a6521d
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-02-21 18:39:03 +0300

    209 упражнение

--- Users.js
+++ Users.js
@@ -1,5 +1,6 @@
-let arr = ['Yunus', 'Mustafaev', 'development', 'programmer', 2000];
+function func() {
+    return ['Yunus', 'Mustafaev', 'development', 'programmer', 2000];
+}
+let [name, surname, department, position, salary] = func();
 
-let [name, surname, department, position, salary] = arr
-
-console.log(name)
+console.log(name, surname)
