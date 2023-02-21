c86d0e3757046d1f5a7825f8b49dad70d814df03
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-02-21 18:45:17 +0300

    213 упражнение

--- Users.js
+++ Users.js
@@ -2,4 +2,9 @@ let arr = ['Yunus', 'Mustafaev', 'development', 'programmer', 2000];
 
 let [name, surname, develop, ...d] = arr
 
-console.log(d)
+let position;
+if (arr[3] !== undefined) {
+	position = arr[3];
+} else {
+	position = 'trainee';
+}
