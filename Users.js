a5a77f3a0e23aeba7b628bb1d0c0fe56ed2c0a6d
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-02-21 18:28:48 +0300

    207 упражнение

--- Users.js
+++ Users.js
@@ -1,4 +1,6 @@
-func(1, 2);
-function func(...rest) {
-	console.log(rest);
+let arrs   = [ [1, 2, 3,] [4, 5, 6], [7, 8, 9] ];
+function merge(...arrs) {
+	return [].concat(...arrs);
 }
+let result = merge([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(result);
