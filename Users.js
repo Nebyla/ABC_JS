ba27c3580aee44ca6c6c0cd7c9da7b2715953a65
эрвин бекиров <tom.margan@mail.ru>
2023-02-22 18:51:22 +0300

    229 упражнение

--- Users.js
+++ Users.js
@@ -1,2 +1,6 @@
-let date = new Date(2025, 12, 1);
-console.log(date.getTime());
+let now  = new Date();
+let date = new Date(1988, 1, 4, 23, 59, 59);
+let data = new Date(2000, 10, 2, 23, 59, 59);
+
+let diff = data.getTime() - date.getTime();
+console.log(diff / (1000 * 60 * 60));
