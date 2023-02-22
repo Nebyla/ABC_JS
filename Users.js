4070e14ba7581fe0dd97f2e756f43fd436d1f5da
эрвин бекиров <tom.margan@mail.ru>
2023-02-22 22:20:11 +0300

    236 упражнение

--- Users.js
+++ Users.js
@@ -1,10 +1,14 @@
-let now  = new Date();
-let year = now.getFullYear();
-
-for (let month = 0; month <= 11; month++) {
-	let date = new Date(year, month, 1);
-	
-	if (date.getDay() == 0) {
-		console.log(year + '-' + month + '-1');
-	}
+function isLeap(){
+    if (new Date().getFullYear() == 12) {
+	    return true;
+    } 
+    else {
+	    return false;
+    }
+}
+if (isLeap == true){
+    console.log("Високосный")
+}
+else{
+    console.log('Обычный')
 }
