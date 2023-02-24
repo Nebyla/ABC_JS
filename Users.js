98a22aa9a3253ea378be075f2f0d99e9615f4f89
эрвин бекиров <tom.margan@mail.ru>
2023-02-24 16:08:12 +0300

    247 упражнение

--- Users.js
+++ Users.js
@@ -1,12 +1,3 @@
-let button1 = document.querySelector('#elem');
-let button = document.querySelector('#elem1');
-
-button1.addEventListener('mouseover', function() {
-	console.log('1');
-});
-button.addEventListener('mouseout', function() {
-	console.log('2');
-});
-button.addEventListener('dblclick', function() {
-	console.log('22');
-});
+let elem = document.querySelector('#elem');
+console.log(elem.textContent);
+elem.textContent = '!!!';
