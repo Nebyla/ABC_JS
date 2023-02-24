4282f4bfea06497b08971d1ad2f4246e9e2d9dad
эрвин бекиров <tom.margan@mail.ru>
2023-02-24 16:13:02 +0300

    248 упражнение

--- Users.js
+++ Users.js
@@ -1,3 +1,10 @@
-let elem = document.querySelector('#elem');
-console.log(elem.textContent);
-elem.textContent = '!!!';
+let button1 = document.querySelector('#elem');
+let button = document.querySelector('#elem1');
+
+console.log(elem.innerHTML)
+elem.innerHTML = '<i> Hello </i>'
+
+function func(){
+	console.log(elem.innerHTML)
+}
+button1.addEventListener('click', func)
