46c127bb7fb9f8e9b64ccebe8e498ce6802a94f7
эрвин бекиров <tom.margan@mail.ru>
2023-02-24 15:24:54 +0300

    243 упражнение

--- Users.js
+++ Users.js
@@ -1,16 +1,12 @@
+let button1 = document.querySelector('#button');
+button.addEventListener('click', func1);
+let button2 = document.querySelector('#button');
+button.addEventListener('click', func2);
 
-let button1 = document.querySelector('#button1');
-let button2 = document.querySelector('#button2');
-let button3 = document.querySelector('#button3');
+function func1() {
+	console.log(1);
+}
 
-button1.addEventListener('click', function() {
-	console.log('Number1');
-});
-
-button2.addEventListener('click', function() {
-	console.log('Number2');
-});
-
-button3.addEventListener('click', function() {
-	console.log('Number3');
-});
+function func2() {
+	console.log(2);
+}
