d9c8320006972f9cf15745a59c1960172bb3dbce
эрвин бекиров <tom.margan@mail.ru>
2023-02-26 16:05:02 +0300

    258 упражнение

--- Users.js
+++ Users.js
@@ -1,9 +1,9 @@
-let elems = document.querySelectorAll('.www');
-let but = document.querySelector('#but')
-let i = 0;
-but.addEventListener('click', function func(){
-	for (let elem of elems) {
-		elem.textContent = elem.textContent + elem.textContent;
-		console.log(elem.textContent)
-	}
-})
+let divs = document.querySelectorAll('div');
+
+for (let div of divs) {
+	div.addEventListener('click', func);
+}
+
+function func() {
+	this.textContent++;
+}
