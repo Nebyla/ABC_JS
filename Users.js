7509def5edc01783cbaad3f64494c42cc028c822
эрвин бекиров <tom.margan@mail.ru>
2023-02-26 16:37:06 +0300

    262 упражнение

--- Users.js
+++ Users.js
@@ -1,10 +1,2 @@
-let elems = document.querySelectorAll('li');
-
-for (let elem of elems) {
-	elem.addEventListener('click', func);
-}
-
-function func() {
-	console.log(this.textContent);
-	this.textContent++;
-}
+let elem = document.querySelector('#elem');
+console.log(elem.hasAttribute('value'));
