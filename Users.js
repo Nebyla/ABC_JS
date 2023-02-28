07b4be14fe50bb4626ad980c5acc90a03c25148b
эрвин бекиров <tom.margan@mail.ru>
2023-02-28 14:51:26 +0300

    269 упражнение

--- Users.js
+++ Users.js
@@ -1,3 +1,5 @@
-let parent = document.querySelector('#parent');
-let elems1 = parent.querySelectorAll('.www')
-let elems2 = parent.querySelectorAll('.ggg')
+let elem = document.querySelector('#elem');
+
+elem.addEventListener('click', function func(){
+	elem.textContent += ' ' + elem.dataset.text;
+})
