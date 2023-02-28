e515950e645793d7d3a8f3c61f43d9086672f923
эрвин бекиров <tom.margan@mail.ru>
2023-02-28 15:20:45 +0300

    276 упражнение

--- Users.js
+++ Users.js
@@ -1,6 +1,10 @@
 let elem = document.querySelector('#elem');
 let button = document.querySelector('#but')
-
+let button1 = document.querySelector('#but1')
+console.log(elem.checked);
 button.addEventListener('click', function func(){
-	elem.disabled = !elem.disabled
+	elem.checked= !elem.disabled
+})
+button1.addEventListener('click', function func(){
+	elem.checked= elem.disabled
 })
