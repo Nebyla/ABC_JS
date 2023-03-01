09efa8af7c5d00329a053ab52c8547a19ed42322
эрвин бекиров <tom.margan@mail.ru>
2023-03-01 16:38:22 +0300

    281 упражнение

--- Users.js
+++ Users.js
@@ -1,8 +1,16 @@
-let elem = document.querySelector('#elem');
+let elem   = document.querySelector('#elem');
+let elem1   = document.querySelector('#elem1');
 
-elem.addEventListener('input', function() {
-	if (this.value.length > 5) {
-    	console.log(elem.value);
+
+elem.addEventListener('input', function func(){
+	if (elem.value.length === 2){
+		elem1.focus();
 	}
-		
-});
+})
+
+elem1.addEventListener('input', function func1(){
+	if (elem1.value.length === 2){
+		console.log('ggg')
+		elem1.blur();
+	}
+})
